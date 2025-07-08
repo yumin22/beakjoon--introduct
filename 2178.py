d9d9539b51f7ada884f2633ac49796ce428330e4
yumin22 <22joomin@naver.com>
2025-07-08 15:30:44 +0900

    [7/8] 2178번 미로 탐색

---
+++ 2178.py
@@ -0,0 +1,42 @@
+# 미로 탐색 (실버I)
+
+# N×M크기의 배열로 표현되는 미로가 있다.
+# 미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 
+# 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 
+# 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+# 위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+from collections import deque
+
+N, M = map(int, input().split())
+
+maze = []
+
+for _ in range(N):
+    maze.append(list(map(int, input())))
+
+def sol(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny <0 or ny >= M:
+                continue
+            if maze[nx][ny] == 0:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y]+1
+                queue.append((nx, ny))
+
+    return maze[N-1][M-1]
+
+print(sol(0,0))
